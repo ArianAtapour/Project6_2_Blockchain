3197d05500c179529f6005cd447c2d1cfc8ae191
EvaldN <91316779+EvaldN@users.noreply.github.com>
2023-05-23 00:07:41 +0200

    Supply chain bare bones
    Really needs to have its methods combined but I didn't manage to do that yet

--- SupplyChain/src/app/app.component.ts
+++ SupplyChain/src/app/app.component.ts
@@ -5,6 +5,73 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'SupplyChain';
+  senderGMaster : string = "";
+  senderFinancier : string = "";
+  senderBuyer : string = "";
+  senderShipper : string = "";
+  senderProducer : string = "";
+  receiverGMaster : string = "";
+  receiverBuyer : string = "";
+  receiverShipper : string = "";
+  receiverProducer : string = "";
+  receiverFinancier : string = "";
+
+  goodNumber : number = 0;
+  badNumber : number = 0;
+  //Text transfer methods, all separate because I didn't figure it out yet
+  GMasterToBuyer()
+  {
+    this.receiverBuyer += this.senderGMaster + "\n";
+  }
+  BuyerToShipper()
+  {
+    this.receiverShipper += this.senderBuyer + "\n";
+  }
+  ShipperToProducer()
+  {
+    this.receiverProducer += this.senderShipper + "\n";
+  }
+  ProducerToBuyer()
+  {
+    this.receiverBuyer += this.senderProducer + "\n";
+  }
+  BuyerToFinancier()
+  {
+    this.receiverFinancier += this.senderBuyer + "\n";
+  }
+  ShipperToFinancier()
+  {
+    this.receiverFinancier += this.senderShipper + "\n";
+  }
+  ProducerToFinancier()
+  {
+    this.receiverFinancier += this.senderProducer + "\n";
+  }
+  FinancierToBuyer()
+  {
+    this.receiverBuyer += this.senderFinancier + "\n";
+  }
+  BuyerToClient()
+  {
+    this.receiverGMaster += this.senderBuyer;
+  }
+  FinancierToShipper()
+  {
+    this.receiverShipper += this.senderFinancier + "\n";
+  }
+  FinancierToProducer()
+  {
+    this.receiverProducer += this.senderFinancier + "\n";
+  }
+  ProductGood()
+  {
+    this.goodNumber++;
+  }
+  ProductBad()
+  {
+    this.badNumber++;
+  }
 }
