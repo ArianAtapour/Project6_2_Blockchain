9724316c51390322249a8e83d853bf5172a1bd06
Polina <91316935+Polinalavender@users.noreply.github.com>
2023-06-01 13:40:37 +0200

    add part of main page

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  goToRules() {
+    this.router.navigate(['/rules-page']);
+  }
+
+  isDropdownOpen = false;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
